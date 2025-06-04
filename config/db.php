97207758318c844ef445560bc358a5952ecf6dfa
MrLuciana <mrbiiigs17@gmail.com>
2025-06-04 21:17:58 +0700

    update database link host

--- config/db.php
+++ config/db.php
@@ -1,18 +1,18 @@
 <?php
 // กำหนดค่าารเชื่อมต่อฐานข้อมูล
-// define('DB_HOST', 'punsib.mrluciana.com');
-// define('DB_USER', 'mrlu_punsib');
-// define('DB_PASS', '47Gexr43@');
-// define('DB_NAME', 'punsib_online');
+define('DB_HOST', 'punsib.mrluciana.com');
+define('DB_USER', 'mrlu_punsib');
+define('DB_PASS', '47Gexr43@');
+define('DB_NAME', 'punsib_online');
 
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'punsib');
+// define('DB_HOST', 'localhost');
+// define('DB_USER', 'root');
+// define('DB_PASS', '');
+// define('DB_NAME', 'punsib');
 
 // กำหนด URL ของเว็บไซต์
-// define('BASE_URL', 'http://punsib.mrluciana.com/');
-define('BASE_URL', 'http://localhost/punsib-online/');
+define('BASE_URL', 'http://punsib.mrluciana.com/');
+// define('BASE_URL', 'http://localhost/punsib-online/');
 define('ADMIN_URL', BASE_URL . 'admin/');
 
 // เริ่ม Session
